d34c2504a36315409c66d7159d9557288b111b35
Antonio Germano <816290+antoniogermano@users.noreply.github.com>
2022-11-11 21:50:11 -0300

    Improves widgets

--- Widgets/Widgets.swift
+++ Widgets/Widgets.swift
@@ -10,69 +10,91 @@ import SwiftUI
 import Intents
 
 struct Provider: IntentTimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+    
+    static let event: Event = {
+        let event = Event(context: PersistenceController.preview.container.viewContext)
+        event.title = "My awesome event"
+        event.eventDescription = "Event description, which might be big so we have a somewhat lengthy description here, one that probably will break the window size for all platforms.\nMust be multiline as well!\nSuch description\nMany lines"
+        event.date = Date(timeInterval: 150000, since: Date.now)
+        // max TimeInterval: 15926483100000
+        return event
+    }()
+    
+    var dateForPeriod: Date {
+        let components = DateComponents(second: 150000) //FIXME: biggest date bug? After 2150000000 it begins going down
+        return Calendar.current.date(byAdding: components, to: Date.now)!
     }
-
-    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), configuration: configuration)
+    
+    func placeholder(in context: Context) -> EventEntry {
+        EventEntry(event: Provider.event, date: dateForPeriod, configuration: ConfigurationIntent())
+    }
+    
+    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (EventEntry) -> ()) {
+        let entry = EventEntry(event: Provider.event, date: Date.now, configuration: configuration)
         completion(entry)
     }
-
+    
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
-            entries.append(entry)
-        }
-
+        var entries: [EventEntry] = [EventEntry(event: Provider.event, date: Date.now, configuration: configuration)]
+        
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
 }
 
-struct SimpleEntry: TimelineEntry {
-    //let title: String
+struct EventEntry: TimelineEntry {
+    let event: Event
+    var title: String { event.title ?? "Unnamed event" }
+    var description: String { event.eventDescription ?? "" }
+    var eventDate: Date { event.date ?? Date.now }
+
     let date: Date
     let configuration: ConfigurationIntent
 }
 
-struct WidgetsEntryView : View {
+struct EventWidgetView : View {
     @Environment(\.widgetFamily) var family
     @Environment(\.isLuminanceReduced) var isLuminanceReduced
     var entry: Provider.Entry
-
+    
     @ViewBuilder
     var body: some View {
-        ZStack {
-            switch family {
-            case .systemSmall: defaultView
-            case .systemMedium: defaultView
-            case .systemLarge: defaultView
-            case .systemExtraLarge: defaultView
-            case .accessoryCircular: accessoryCircular
-            case .accessoryRectangular: accessoryRectangular
-            case .accessoryInline: accessoryInline
-            @unknown default: defaultView
-            }
+        switch family {
+        case .systemSmall: defaultView
+        case .systemMedium: defaultView
+        case .systemLarge: defaultView
+        case .systemExtraLarge: defaultView
+        case .accessoryCircular: accessoryCircular
+        case .accessoryRectangular: accessoryRectangular
+        case .accessoryInline: accessoryInline
+        @unknown default: defaultView
         }
+    }
+    
+    var dateRange: ClosedRange<Date> {
+        if Date.now.compare(entry.eventDate).rawValue < 0 {
+            return Date.now...entry.eventDate
+        }
+        return entry.eventDate...Date.now
+    }
+    
+    var eventHourComponent: String {
+        let dateComponents = Calendar.current.dateComponents([.hour], from: Date.now, to: entry.eventDate)
+        let hourString = dateComponents.value(for: .hour)?.description
+        return hourString ?? ""
     }
     
     var accessoryCircular: some View {
         ZStack {
 //            if #available(iOSApplicationExtension 16.0, *) {
-                AccessoryWidgetBackground()
-                ProgressView(timerInterval: dateRange, label: {
-                    Text("Event title here")
-                }, currentValueLabel: {
-                    Text(dateForPeriod, style: .relative)
-                        .font(.subheadline)
-                })
-                .progressViewStyle(.circular)
+            AccessoryWidgetBackground()
+            ProgressView(timerInterval: dateRange, label: {
+                Text(entry.title)
+            }, currentValueLabel: {
+                Text(entry.eventDate, style: .relative)
+                    .font(.subheadline)
+            })
+            .progressViewStyle(.circular)
 //            } else {
 //                VStack(alignment: .center) {
 //                    HStack(alignment: .center) {
@@ -89,56 +111,57 @@ struct WidgetsEntryView : View {
         }
     }
     
-    var dateRange: ClosedRange<Date> {
-        return Date.now...dateForPeriod
-    }
-    
-    var dateForPeriod: Date {
-        let components = DateComponents(second: 100) //FIXME: biggest date bug? After 2150000000 it begins going down
-        return Calendar.current.date(byAdding: components, to: Date.now)!
-    }
-    
     var defaultView: some View {
-        return VStack(alignment: .leading) {
-            Text("Event title that is large so that it can not possibly fit into the system small widget.")
-                .truncationMode(.middle)
-            Text(dateForPeriod, style: .relative)
-                .font(.largeTitle)
-                .foregroundColor(.orange)
-                .lineLimit(2)
-                .minimumScaleFactor(0.5)
-        }
+//        ZStack {
+//            AccessoryWidgetBackground()
+            VStack(alignment: .leading) {
+                Spacer()
+                Text(entry.title)
+                    .truncationMode(.middle)
+                Text(entry.eventDate, style: .relative)
+                    .font(.largeTitle)
+                    .foregroundColor(.orange)
+                    .lineLimit(2)
+                    .minimumScaleFactor(0.5)
+                Spacer()
+            }
+//        }
         .padding()
+//        .background(ContainerRelativeShape().fill(Color.init(.sRGB, red: 0.89, green: 0.89, blue: 0.89, opacity: 0.9)))
     }
     
     var accessoryInline: some View {
         ViewThatFits {
-            Text("Event title: \(dateForPeriod, style: .relative)")
-            Text("Event title: \(dateForPeriod, style: .offset)")
-            Text("(8h) Event title")
+            Text("\(entry.title): \(entry.eventDate, style: .relative)")
+            Text("\(entry.title): \(entry.eventDate, style: .offset)")
+            if eventHourComponent.isEmpty {
+                Text(entry.title)
+            } else {
+                Text("(\(eventHourComponent)h) \(entry.title)")
+            }
         }
     }
     
     var accessoryRectangular: some View {
         VStack(alignment: .leading) {
-            Text("Event title")
+            Text(entry.title)
                 .font(.headline)
                 .widgetAccentable()
-            Text("Event description")
+            Text(entry.description)
                 .foregroundColor(.secondary)
                 .font(.footnote)
-            Text(entry.date, style: .relative)
+            Text(entry.eventDate, style: .relative)
                 .foregroundColor(.accentColor)
         }
     }
 }
 
 struct Widgets: Widget {
-    let kind: String = "Widgets"
-
+    let kind: String = "Event"
+    
     var body: some WidgetConfiguration {
         IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
-            WidgetsEntryView(entry: entry)
+            EventWidgetView(entry: entry)
         }
         .configurationDisplayName("Events")
         .description("See how much time remaining for your events")
@@ -171,9 +194,11 @@ struct Widgets_Previews: PreviewProvider {
     
     static var previews: some View {
         ForEach(families, id: \.self) { family in
-            WidgetsEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
-                .previewContext(WidgetPreviewContext(family: family))
-                .previewDisplayName(family.description)
+            Group {
+                EventWidgetView(entry: EventEntry(event: Provider.event, date: Date.now, configuration: ConfigurationIntent()))
+                    .previewContext(WidgetPreviewContext(family: family))
+                    .previewDisplayName(family.description)
+            }
         }
     }
 }
