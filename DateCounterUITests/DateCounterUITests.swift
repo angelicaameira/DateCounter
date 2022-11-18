422c541c57b201b8674de7c0cef8749465d1b55d
Antonio Germano <816290+antoniogermano@users.noreply.github.com>
2022-11-18 10:13:00 -0300

    Fixes UI test to make it work on iOS 15 and 16, iPhone and iPad

--- DateCounterUITests/DateCounterUITests.swift
+++ DateCounterUITests/DateCounterUITests.swift
@@ -23,16 +23,20 @@ final class DateCounterUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+#if os(iOS)
     func testEvenHasRequiredTitleField_CancelEditing() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
         
+        if UIDevice.current.userInterfaceIdiom == .pad {
+            XCUIDevice.shared.orientation = .landscapeRight
+        }
+        
         let eventsNavigationBar = app.navigationBars["Events"]
         eventsNavigationBar.buttons["Add Event"].tap()
-        
-        let tablesQuery = app.tables
-        let eventNameTextField = tablesQuery/*@START_MENU_TOKEN@*/.textFields["Event name"]/*[[".cells[\"Event name\"].textFields[\"Event name\"]",".textFields[\"Event name\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+
+        let eventNameTextField = app/*@START_MENU_TOKEN@*/.textFields["Event name"]/*[[".cells[\"Event name\"].textFields[\"Event name\"]",".textFields[\"Event name\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         eventNameTextField.tap()
 
         let tKey = app/*@START_MENU_TOKEN@*/.keys["T"]/*[[".keyboards.keys[\"T\"]",".keys[\"T\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
@@ -43,34 +47,53 @@ final class DateCounterUITests: XCTestCase {
         sKey.tap()
         let tKey2 = app/*@START_MENU_TOKEN@*/.keys["t"]/*[[".keyboards.keys[\"t\"]",".keys[\"t\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
         tKey2.tap()
-        eKey.tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.datePickers["Date"].collectionViews.staticTexts["26"]/*[[".cells[\"Month, Show year picker, novembro de 2022, Previous Month, Next Month, SEG., TER., QUA., QUI., SEX., SÁB., DOM., terça-feira, 1 de novembro, 1, quarta-feira, 2 de novembro, 2, quinta-feira, 3 de novembro, 3, sexta-feira, 4 de novembro, 4, sábado, 5 de novembro, 5, domingo, 6 de novembro, 6, segunda-feira, 7 de novembro, 7, terça-feira, 8 de novembro, 8, quarta-feira, 9 de novembro, 9, quinta-feira, 10 de novembro, 10, sexta-feira, 11 de novembro, 11, sábado, 12 de novembro, 12, domingo, 13 de novembro, 13, segunda-feira, 14 de novembro, 14, terça-feira, 15 de novembro, 15, quarta-feira, 16 de novembro, 16, quinta-feira, 17 de novembro, 17, Today, sexta-feira, 18 de novembro, 18, sábado, 19 de novembro, 19, domingo, 20 de novembro, 20, segunda-feira, 21 de novembro, 21, terça-feira, 22 de novembro, 22, quarta-feira, 23 de novembro, 23, quinta-feira, 24 de novembro, 24, sexta-feira, 25 de novembro, 25, sábado, 26 de novembro, 26, domingo, 27 de novembro, 27, segunda-feira, 28 de novembro, 28, terça-feira, 29 de novembro, 29, quarta-feira, 30 de novembro, 30, segunda-feira, 28 de novembro, terça-feira, 29 de novembro, 07:59, 07:59\"].datePickers[\"Date\"].collectionViews",".buttons[\"sábado, 26 de novembro\"].staticTexts[\"26\"]",".staticTexts[\"26\"]",".datePickers[\"Date\"].collectionViews"],[[[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0,0]]@END_MENU_TOKEN@*/.tap()
+        
+        let day26 = app.staticTexts["26"]
+        if day26.isHittable {
+            day26.tap()
+        }
 
         let saveButton = app.navigationBars["Add event"].buttons["Save"]
         saveButton.tap()
         
-        app.tables.cells.firstMatch.tap()
+        app.cells.containing(NSPredicate(format: "label contains[c] %@", "Test")).firstMatch.tap()
         
-        let testeNavigationBar = app.navigationBars["Teste"]
-        testeNavigationBar.buttons["Edit this event"].tap()
-        tablesQuery/*@START_MENU_TOKEN@*/.textFields["Event name"]/*[[".cells.textFields[\"Event name\"]",".textFields[\"Event name\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-        
+        let testNavigationBar = app.navigationBars["Test"]
+        testNavigationBar.buttons["Edit this event"].tap()
+        app/*@START_MENU_TOKEN@*/.textFields["Event name"]/*[[".cells.textFields[\"Event name\"]",".textFields[\"Event name\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+
         let deleteKey = app/*@START_MENU_TOKEN@*/.keys["delete"]/*[[".keyboards",".keys[\"apagar\"]",".keys[\"delete\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/
         deleteKey.tap()
         deleteKey.tap()
         deleteKey.tap()
         deleteKey.tap()
         deleteKey.tap()
-        
+
         let editEventNavigationBar = app.navigationBars["Edit event"]
         editEventNavigationBar.buttons["Save"].tap()
         app.alerts["An error occurred when adding event"].scrollViews.otherElements.buttons["OK"].tap()
         editEventNavigationBar.buttons["Cancel"].tap()
-        testeNavigationBar.buttons["Events"].tap()
         
-        XCTAssert(app.tables.cells.firstMatch.exists)
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            testNavigationBar.buttons["Events"].tap()
+        }
+
+        XCTAssert(app.cells.containing(NSPredicate(format: "label contains[c] %@", "Test")).firstMatch.exists)
+        
+        eventsNavigationBar/*@START_MENU_TOKEN@*/.buttons["Edit"]/*[[".otherElements[\"Edit\"].buttons[\"Edit\"]",".buttons[\"Edit\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        let deleteLeadingButton = app.cells.buttons["Delete "]
+        if deleteLeadingButton.exists {
+            deleteLeadingButton.firstMatch.tap() // iOS 15
+        } else {
+            app.cells.otherElements.containing(.image, identifier:"remove").firstMatch.tap() // iOS 16
+        }
+        app.buttons["Delete"].tap()
+        eventsNavigationBar/*@START_MENU_TOKEN@*/.buttons["Done"]/*[[".otherElements[\"Done\"].buttons[\"Done\"]",".buttons[\"Done\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        XCTAssert(app.cells.containing(NSPredicate(format: "label contains[c] %@", "Test")).firstMatch.exists == false)
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
+#endif
 
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
