1864140c5327e7ca5d78b7ad7dd02e934c110913
Angelica Meira <angel-am@hotmail.com>
2022-11-30 13:44:05 -0300

    Melhora design da tela de adição no Watch

--- DateCounter/Views/ManageEventView.swift
+++ DateCounter/Views/ManageEventView.swift
@@ -21,6 +21,7 @@ struct ManageEventView: View {
     @State private var showError = false
     @State private var errorMessage = "No error"
     @State var event: Event? = nil
+    @State private var showTimePicker = false
     private var navigationBarTitle: String {
         event == nil ? "Add event" : "Edit event"
     }
@@ -129,9 +130,41 @@ struct ManageEventView: View {
                 TextField("Description", text: $eventDescription)
             }
             Section {
-                DatePicker("Date time", selection: $date)
-                DatePicker("Time", selection: $date)
+                NavigationLink {
+                    DateInputView(selection: $date)
+                } label: {
+                    VStack(alignment: .leading) {
+                        Text("Date")
+                        Text(date, style: .date)
+                            .font(.footnote)
+                            .foregroundColor(.secondary)
+                    }
+                }
+                
+                Button {
+                    showTimePicker = true
+                } label: {
+                    VStack(alignment: .leading) {
+                        Text("Time")
+                        Text(date, style: .time)
+                            .font(.footnote)
+                            .foregroundColor(.secondary)
+                    }
+                }
             }
+            // sheet or fullScreenCover?
+            .sheet(isPresented: $showTimePicker, content: {
+                TimeInputView(selection: $date)
+                    .edgesIgnoringSafeArea(.all)
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Done") {
+                                showTimePicker = false
+                            }
+                            .foregroundColor(.orange)
+                        }
+                    }
+            })
         }
     }
 #endif
